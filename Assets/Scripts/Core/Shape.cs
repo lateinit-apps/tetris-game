505992e9d9afb73373ee49bca5347c4a8fd8bc78
Artem Bondar <artem.bondar.dev@gmail.com>
2019-11-20 20:44:58 +0300

    Implement shape class

--- Assets/Scripts/Core/Shape.cs
+++ Assets/Scripts/Core/Shape.cs
@@ -1,18 +1,59 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Shape : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public bool canRotate = true;
+
+    private void Move(Vector3 moveDirection)
     {
-        
+        transform.position += moveDirection;
+    }
+
+    public void MoveLeft()
+    {
+        Move(new Vector3(-1, 0, 0));
+    }
+
+    public void MoveRight()
+    {
+        Move(new Vector3(1, 0, 0));
+    }
+
+    public void MoveUp()
+    {
+        Move(new Vector3(0, 1, 0));
+    }
+
+    public void MoveDown()
+    {
+        Move(new Vector3(0, -1, 0));
+    }
+
+    public void RotateRight()
+    {
+        if (canRotate)
+        {
+            transform.Rotate(0, 0, -90);
+        }
+    }
+    
+    public void RotateLeft()
+    {
+        if (canRotate)
+        {
+            transform.Rotate(0, 0, 90);
+        }
+    }
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+
     }
 }
