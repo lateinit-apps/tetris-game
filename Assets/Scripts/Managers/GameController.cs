711faf4602fc80cb6597c8f823305923eaed751c
Artem Bondar <artem.bondar.dev@gmail.com>
2019-11-20 21:19:05 +0300

    Add raw implementation of game controller class

--- Assets/Scripts/Managers/GameController.cs
+++ Assets/Scripts/Managers/GameController.cs
@@ -8,11 +8,22 @@ public class GameController : MonoBehaviour
 
     private void Start()
     {
-        
+        gameBoard = GameObject.FindObjectOfType<Board>();
+        spawner = GameObject.FindObjectOfType<Spawner>();
+
+        if (!gameBoard)
+        {
+            Debug.Log("WARNING! There is no game board defined!");
+        }
+
+        if (!spawner)
+        {
+            Debug.Log("WARNING! There is no spawner defined!");
+        }
     }
 
     private void Update()
     {
-        
+
     }
 }
