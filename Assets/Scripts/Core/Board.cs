8af3fdc73f0a3e83c49221e6a49594343ffc8ac4
Artem Bondar <artem.bondar.dev@gmail.com>
2019-11-20 15:03:14 +0300

    Implement board class

--- Assets/Scripts/Core/Board.cs
+++ Assets/Scripts/Core/Board.cs
@@ -1,18 +1,51 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Board : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Transform emptySprite;
+
+    public int height = 30;
+    public int width = 10;
+
+    public int header = 8;
+
+    private Transform [,] grid;
+    
+    private void Awake()
     {
-         
+        grid = new Transform[width, height];        
+    }
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        DrawEmptyCells();
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
         
+    }
+
+    private void DrawEmptyCells()
+    {
+        if (emptySprite != null)
+        {
+            for (int y = 0; y < height - header; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    Transform clone;
+                    clone = Instantiate<Transform>(emptySprite, new Vector3(x, y, 0), Quaternion.identity);
+                    clone.name = "Board Space ( x = " + x.ToString() + " , y = " + y.ToString() + " )";
+                    clone.transform.parent = transform;
+                }
+            }
+        }
+        else
+        {
+            Debug.Log("WARNING! Please assign the emptySprite object!");
+        }
     }
 }
