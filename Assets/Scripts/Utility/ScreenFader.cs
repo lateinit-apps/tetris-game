a32f0da84b512b055d808a1e40417b4302c1de6a
Artem Bondar <artem.bondar.dev@gmail.com>
2019-11-21 16:58:33 +0300

    Create screen fader

---
+++ Assets/Scripts/Utility/ScreenFader.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+using System.Collections;
+
+[RequireComponent(typeof(MaskableGraphic))]
+public class ScreenFader : MonoBehaviour
+{
+    public float startAlpha = 1f;
+    public float targetAlpha = 0f;
+    public float delay = 0f;
+    public float timeToFade = 1f;
+
+    private float increment;
+    private float currrentAlpha;
+
+    private MaskableGraphic graphic;
+    private Color originalColor;
+
+    private void Start()
+    {
+        graphic = GetComponent<MaskableGraphic>();
+
+        originalColor = graphic.color;
+        currrentAlpha = startAlpha;
+
+        Color tempColor =
+            new Color(originalColor.r, originalColor.g, originalColor.b, currrentAlpha);
+        graphic.color = tempColor;
+
+        increment = (targetAlpha - startAlpha) / timeToFade * Time.deltaTime;
+
+        StartCoroutine(FadeRoutine());
+    }
+
+    IEnumerator FadeRoutine()
+    {
+        yield return new WaitForSeconds(delay);
+
+        while (Mathf.Abs(targetAlpha - currrentAlpha) > 0.01f)
+        {
+            yield return new WaitForEndOfFrame();
+
+            currrentAlpha += increment;
+
+            Color tempColor =
+                new Color(originalColor.r, originalColor.g, originalColor.b, currrentAlpha);
+            graphic.color = tempColor;
+        }
+    }
+}
