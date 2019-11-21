888666ff10ce8d0df2c7064b364ed32a3ef30e50
Artem Bondar <artem.bondar.dev@gmail.com>
2019-11-21 14:39:46 +0300

    Implement sound manager and backround music

---
+++ Assets/Scripts/Managers/SoundManager.cs
@@ -0,0 +1,72 @@
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    public bool musicEnabled = true;
+    public bool fxEnabled = true;
+
+    [Range(0, 1)]
+    public float musicVolume = 1.0f;
+
+    [Range(0, 1)]
+    public float fxVolume = 1.0f;
+
+    public AudioClip clearRowSound;
+    public AudioClip moveSound;
+    public AudioClip dropSound;
+    public AudioClip gameOverSound;
+
+    public AudioSource musicSource;
+
+    public AudioClip[] musicClips;
+
+    private AudioClip randomMusicClip;
+
+    private void Start()
+    {
+        randomMusicClip = GetRandomClip(musicClips);
+        PlayBackgroundMusic(randomMusicClip);
+    }
+
+    public AudioClip GetRandomClip(AudioClip[] clips)
+    {
+        AudioClip randomClip = clips[Random.Range(0, clips.Length)];
+        return randomClip;
+    }
+
+    public void PlayBackgroundMusic(AudioClip musicClip)
+    {
+        if (!musicEnabled || !musicClip || !musicSource)
+        {
+            return;
+        }
+
+        musicSource.Stop();
+        musicSource.clip = musicClip;
+        musicSource.volume = musicVolume;
+        musicSource.loop = true;
+        musicSource.Play();
+    }
+
+    public void ToggleMusic()
+    {
+        musicEnabled = !musicEnabled;
+        UpdateMusic();
+    }
+
+    private void UpdateMusic()
+    {
+        if (musicSource.isPlaying != musicEnabled)
+        {
+            if (musicEnabled)
+            {
+                randomMusicClip = GetRandomClip(musicClips);
+                PlayBackgroundMusic(randomMusicClip);
+            }
+            else
+            {
+                musicSource.Stop();
+            }
+        }
+    }
+}
