738e0fbd5b1fba932f44c77607903e127e80db47
Artem Bondar <artem.bondar.dev@gmail.com>
2019-11-22 13:06:32 +0300

    Add swipe diagnostic

--- Assets/Scripts/Managers/TouchController.cs
+++ Assets/Scripts/Managers/TouchController.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class TouchController : MonoBehaviour
 {
@@ -10,12 +11,50 @@ public class TouchController : MonoBehaviour
 
     private int minSwipeDistance = 20;
 
+    public Text diagnosticText1;
+    public Text diagnosticText2;
+
+    public bool useDiagnostic = false;
+
     private void OnSwipe()
     {
         if (SwipeEvent != null)
         {
             SwipeEvent(touchMovement);
         }
+    }
+
+    private void Diagnostic(string text1, string text2)
+    {
+        diagnosticText1.gameObject.SetActive(useDiagnostic);
+        diagnosticText2.gameObject.SetActive(useDiagnostic);
+
+        if (diagnosticText1 && diagnosticText2)
+        {
+            diagnosticText1.text = text1;
+            diagnosticText2.text = text2;
+        }
+    }
+
+    private string SwipeDiagnostic(Vector2 swipeMovement)
+    {
+        string direction = "";
+
+        if (Mathf.Abs(swipeMovement.x) > Mathf.Abs(swipeMovement.y))
+        {
+            direction = swipeMovement.x >= 0 ? "right" : "left";
+        }
+        else
+        {
+            direction = swipeMovement.y >= 0 ? "up" : "down";
+        }
+
+        return direction;
+    }
+
+    private void Start()
+    {
+        Diagnostic("", "");
     }
 
     private void Update()
@@ -27,6 +66,7 @@ public class TouchController : MonoBehaviour
             if (touch.phase == TouchPhase.Began)
             {
                 touchMovement = Vector2.zero;
+                Diagnostic("", "");
             }
             else if (touch.phase == TouchPhase.Moved || touch.phase == TouchPhase.Stationary)
             {
@@ -35,6 +75,8 @@ public class TouchController : MonoBehaviour
                 if (touchMovement.magnitude > minSwipeDistance)
                 {
                     OnSwipe();
+                    Diagnostic("Swipe detected",
+                               touchMovement.ToString() + " " + SwipeDiagnostic(touchMovement));
                 }
             }
         }
